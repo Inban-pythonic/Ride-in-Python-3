1c014b59d2f1f73789fcfbcea1da6aaed64e2a81
INBAN <32680971+Inban-pythonic@users.noreply.github.com>
2017-11-08 05:43:41 +0530

    Add files via upload

---
+++ 5 Variables.py
@@ -0,0 +1,52 @@
+"""Variable Names Rules :
+    1) A variable Name must consist of only letters, digits, and/or underscores(_)
+    2) A variable Name must start with a letter or an underscore
+    3) A variable Name should not be a reserved word (keywords)"""
+
+x = 5
+#print(x)
+
+x = 7 * 9 + 13
+#print(x)
+
+x = "A nod's as good as a wink to a blind bat."
+#print(x)
+
+x = int(15/4)-27
+#print(x)
+
+"""x = 2
+y = 3
+print("x =", x)
+print("y =", y)
+print("x * y =", x*y)
+print("x + y =", x+y)"""
+
+x = 2
+y = 3
+#print("x =", x, "and y =", y)
+
+#Swap the values of x and y using z as intermediary storage.
+
+z = x
+x = y
+y = z
+#print("x =", x, "and y =", y)
+
+
+x = 2
+#print(x)
+x = x+3
+#print(x)
+
+
+#print(days_in_a_year)
+days_in_a_year = 365
+
+
+pi = 3.14159265
+radius = 7.5
+height = 8.25
+volume_of_cone = pi * radius * radius * height/3
+#print(volume_of_cone)
+
