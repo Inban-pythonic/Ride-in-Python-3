e2c8ddb7402c26c7d8b0ea89332c610384115734
INBAN <32680971+Inban-pythonic@users.noreply.github.com>
2017-11-08 16:33:59 +0530

    Add files via upload

---
+++ 9 Statement & Comment.py
@@ -0,0 +1,30 @@
+"""#Python Indentation
+
+for i in range(1,11):
+    #print(i)
+    if i == 5:
+        break
+
+if True:
+    #print('Hello')
+    a = 5
+
+
+if True: #print('Hello'); a = 5"""
+
+#Docstring in Python
+
+def double(num):
+    """Function to double the value"""
+    return 2*num
+
+print(double.__doc__)
+
+#Python Comments
+
+#Python supports two types of comments:
+#single line comment  
+#print ("Hello Python")  
+'''''This is 
+multiline comment''' 
+
