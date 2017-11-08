b373ee5474353bdb792cdfaba9e279f59eac9d05
INBAN <32680971+Inban-pythonic@users.noreply.github.com>
2017-11-08 07:08:41 +0530

    Add files via upload

---
+++ 1.6 Python I'O Import.py
@@ -0,0 +1,31 @@
+#print ('This sentence is output to the screen')
+
+a = 5
+#print ('The value of a is', a)
+
+#print(1,2,3,4)
+
+#print(1,2,3,4,sep='*')
+
+#print(1,2,3,4,sep='#',end='&')
+
+x = 5; y = 10
+#print('The value of x is {} and y is {}'.format(x,y))
+
+#print('I love {0} and {1}'.format('bread','butter'))
+
+#print('I love {1} and {0}'.format('bread','butter'))
+
+#print('Hello {name}, {greeting}'.format(greeting = 'Goodmorning', name = 'John'))
+
+
+x = 12.3456789
+#print('The value of x is %3.2f' %x)
+
+#print('The value of x is %3.4f' %x)
+
+
+#Python Input
+
+num = input('Enter a number: ') 
+
