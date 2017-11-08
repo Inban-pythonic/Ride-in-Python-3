4f5642fecb5a846fe23f534b4b6af2e9ffa94e99
INBAN <32680971+Inban-pythonic@users.noreply.github.com>
2017-11-08 22:31:45 +0530

    Add files via upload

---
+++ 1.11 Python Operators.py
@@ -0,0 +1,97 @@
+#Example #1: Arithmetic operators in Python
+
+"""x = 15
+y = 4
+
+# Output: x + y = 19
+print('x + y =',x+y)
+
+# Output: x - y = 11
+print('x - y =',x-y)
+
+# Output: x * y = 60
+print('x * y =',x*y)
+
+# Output: x / y = 3.75
+print('x / y =',x/y)
+
+# Output: x // y = 3
+print('x // y =',x//y)
+
+# Output: x ** y = 50625
+print('x ** y =',x**y)"""
+
+#Example #2: Comparison operators in Python
+
+"""x = 10
+y = 12
+
+# Output: x > y is False
+print('x > y  is',x>y)
+
+# Output: x < y is True
+print('x < y  is',x<y)
+
+# Output: x == y is False
+print('x == y is',x==y)
+
+# Output: x != y is True
+print('x != y is',x!=y)
+
+# Output: x >= y is False
+print('x >= y is',x>=y)
+
+# Output: x <= y is True
+print('x <= y is',x<=y)"""
+
+#Example #3: Logical Operators in Python
+
+"""x = True
+y = False
+
+# Output: x and y is False
+print('x and y is',x and y)
+
+# Output: x or y is True
+print('x or y is',x or y)
+
+# Output: not x is False
+print('not x is',not x)"""
+
+#Example #4: Membership operators in Python
+
+"""x = 'Hello world'
+y = {1:'a',2:'b'}
+
+# Output: True
+print('H' in x)
+
+# Output: True
+    print('hello' not in x)
+
+# Output: True
+print(1 in y)
+
+# Output: False
+print('a' in y)"""
+
+Example #5: Identity operators in Python
+
+"""x1 = 5
+y1 = 5
+x2 = 'Hello'
+y2 = 'Hello'
+x3 = [1,2,3]
+y3 = [1,2,3]
+
+# Output: False
+print(x1 is not y1)
+
+# Output: True
+print(x2 is y2)
+
+# Output: False
+print(x3 is y3)"""
+
+
+
