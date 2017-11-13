ec1fc711deaf0da052044d47332ef909702d6184
INBAN <32680971+Inban-pythonic@users.noreply.github.com>
2017-11-13 12:21:19 +0530

    Add files via upload

---
+++ 2.5 Python Set.py
@@ -0,0 +1,215 @@
+#How to create a set?
+'''#1 set of integers
+my_set = {1, 2, 3}
+print(my_set)'''
+
+#2. set of mixed datatypes
+'''my_set = {1.0, "Hello", (1, 2, 3)}
+print(my_set)'''
+
+#3. Try the following examples as well.
+'''# set do not have duplicates
+# Output: {1, 2, 3, 4}
+my_set = {1,2,3,4,3,2}
+print(my_set)
+
+# set cannot have mutable items
+# here [3, 4] is a mutable list
+# If you uncomment line #12,
+# this will cause an error.
+# TypeError: unhashable type: 'list'
+
+#my_set = {1, 2, [3, 4]}
+
+# we can make set from a list
+# Output: {1, 2, 3}
+my_set = set([1,2,3,2])
+print(my_set)'''
+
+#4. Creating an empty set is a bit tricky
+'''# initialize a with {}
+a = {}
+
+# check data type of a
+# Output: <class 'dict'>
+print(type(a))
+
+# initialize a with set()
+a = set()
+
+# check data type of a
+# Output: <class 'set'>
+print(type(a))'''
+
+#5. How to change a set in Python?
+'''# initialize my_set
+my_set = {1,3}
+print(my_set)
+
+# if you uncomment line 9,
+# you will get an error
+# TypeError: 'set' object does not support indexing
+
+#my_set[0]
+
+# add an element
+# Output: {1, 2, 3}
+my_set.add(2)
+print(my_set)
+
+# add multiple elements
+# Output: {1, 2, 3, 4}
+my_set.update([2,3,4])
+print(my_set)
+
+# add list and set
+# Output: {1, 2, 3, 4, 5, 6, 8}
+my_set.update([4,5], {1,6,8})
+print(my_set)'''
+
+#6. remove elements from a set
+'''# initialize my_set
+my_set = {1, 3, 4, 5, 6}
+print(my_set)
+
+# discard an element
+# Output: {1, 3, 5, 6}
+my_set.discard(4)
+print(my_set)
+
+# remove an element
+# Output: {1, 3, 5}
+my_set.remove(6)
+print(my_set)
+
+# discard an element
+# not present in my_set
+# Output: {1, 3, 5}
+my_set.discard(2)
+print(my_set)
+
+# remove an element
+# not present in my_set
+# If you uncomment line 27,
+# you will get an error.
+# Output: KeyError: 2
+
+#my_set.remove(2)'''
+
+#7. we can remove and return an item using the pop(), clear() method
+
+'''# initialize my_set
+# Output: set of unique elements
+my_set = set("HelloWorld")
+print(my_set)
+
+# pop an element
+# Output: random element
+print(my_set.pop())
+
+# pop another element
+# Output: random element
+my_set.pop()
+print(my_set)
+
+# clear my_set
+#Output: set()
+my_set.clear()
+print(my_set)'''
+
+#8. Python Set Operations
+'''#8.1 Set Union
+# initialize A and B
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+# use | operator
+# Output: {1, 2, 3, 4, 5, 6, 7, 8}
+print(A | B)
+
+#8.2 Set Intersection
+# initialize A and B
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+# use & operator
+# Output: {4, 5}
+print(A & B)
+
+#8.3 Set Difference
+# initialize A and B
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+# use - operator on A
+# Output: {1, 2, 3}
+print(A - B)
+
+#8.4 Set Symmetric Difference
+# initialize A and B
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+# use ^ operator
+# Output: {1, 2, 3, 6, 7, 8}
+print(A ^ B)'''
+
+#9. Other Set Operations
+'''#Set Membership Test
+# initialize my_set
+my_set = set("apple")
+
+# check if 'a' is present
+# Output: True
+print('a' in my_set)
+
+# check if 'p' is present
+# Output: False
+print('p' not in my_set)'''
+
+#10. Python Frozenset
+'''# initialize A and B
+A = frozenset([1, 2, 3, 4])
+B = frozenset([3, 4, 5, 6])'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
